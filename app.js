b324933d34485a13996e4dae0b68d314ffcf3d9b
TejasChalke <tejaschalke66@gmail.com>
2023-10-19 10:24:31 +0530

    renamed file and changed port

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 8081;
+const PORT = 8080;
 
 // Serve static files from the public directory (where your index.html is located)
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server now is running on http://localhost:${PORT}`);
 });
